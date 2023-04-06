628465759cc944724b5380fd1863e4b528faa8eb
Kbusch54 <kevinbusch@comcast.net>
2023-04-06 11:53:26 -0400

    Theseus DAO shell

--- minoan-exchange-nextjs-frontend/app/theseusDao/page.tsx
+++ minoan-exchange-nextjs-frontend/app/theseusDao/page.tsx
@@ -1,9 +1,152 @@
+import Image from "next/image";
 import React from "react";
+import PurposalModal from "../../components/modals/PurposalModal";
+import DaoTransaction from "../../components/tables/DaoTransactions";
+import TheseusTab from "../../components/tabs/TheseusTab";
+import theseus from "../../public/assets/theseus-removed.png";
 
 type Props = {};
 
 const page = (props: Props) => {
-  return <div>page</div>;
+  return (
+    <div className="m-6">
+      <div className="flex flex-row justify-between">
+        <div className="mt-12 ml-24 text-white  ">
+          <h1 className="text-5xl ">Theseus DAO</h1>
+          <div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">0.00</h1>
+              <h3 className="text-xl"> Your Balance</h3>
+            </div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">$0.00</h1>
+              <h3 className="text-xl"> Current Value</h3>
+            </div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">134533</h1>
+              <h3 className="text-xl"> Total Supply</h3>
+            </div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">$9382.02</h1>
+              <h3 className="text-xl"> Total Value</h3>
+            </div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">$6983.39</h1>
+              <h3 className="text-xl"> Loaned Out</h3>
+            </div>
+            <div className="flex flex-col text-center">
+              <h1 className="text-xl md:text-3xl lg:text-5xl">$2398.63</h1>
+              <h3 className="text-xl"> In Vault</h3>
+            </div>
+          </div>
+        </div>
+        <div className="hidden lg:block">
+          <Image alt={"theseus"} src={theseus} height={720} />
+        </div>
+      </div>
+      <div className="flex flex-row justify-center m-8 border-4 border-amber-400">
+        <TheseusTab />
+      </div>
+      <div className="flex flex-wrap justify-evenly text-center gap-y-12 text-white">
+        <div className="rounded-2xl bg-gradient-to-r from-cyan-500 via-red-500 to-amber-500 p-1 shadow-xl">
+          <div className="block rounded-xl bg-slate-800 -z-10 p-4 sm:p-6 lg:p-8">
+            <div className="flex flex-col gap-y-4 ">
+              <h1 className="text-xl">Ranges Allowed</h1>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Minimum Margin Ratio:</h3>
+                <h3>2% - 10%</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Minimum Leverage:</h3>
+                <h3>2X</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Maximum Leverage:</h3>
+                <h3>20X</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Minimum Investment</h3>
+                <h3>$950.00</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Maximum Investment:</h3>
+                <h3>$20,000.00</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Reward %:</h3>
+                <h3>20% - 70%</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Interest Rates:</h3>
+                <h3>1% - 10%</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Interest Period:</h3>
+                <h3>4hrs - 48hrs</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Reward period:</h3>
+                <h3>8hrs - 24hrs</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-2xl bg-gradient-to-r from-cyan-500 via-red-500 to-amber-500 p-1 shadow-xl">
+          <div className="block rounded-xl bg-slate-800 -z-10 p-4 sm:p-6 lg:p-8">
+            <div className="flex flex-col gap-y-4">
+              <h1 className="text-xl">Protocal Stats</h1>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Total USDC Traded:</h3>
+                <h3>$472,973.32</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Outstanding Debt Owned:</h3>
+                <h3>$2,348.84</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Amount in Insurance Fund:</h3>
+                <h3>$47,567.02</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Insurance Fund Cap:</h3>
+                <h3>$50,000.00</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Cummulative DAO Earnings:</h3>
+                <h3>$24,948.00</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Trader Earnings:</h3>
+                <h3>$20,383.05</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Cummulative Pool Earnings:</h3>
+                <h3>$36,837.00</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>Total Number of Pools:</h3>
+                <h3>3</h3>
+              </div>
+              <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
+                <h3>USDC in DAO:</h3>
+                <h3>$394,948.93</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-12 flex flex-col ">
+        <h1 className="text-3xl text-white justify-center text-center mb-2">
+          DAO Purposals
+        </h1>
+        <DaoTransaction />
+      </div>
+
+      <div className="flex justify-center">
+        <PurposalModal />
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ minoan-exchange-nextjs-frontend/components/tabs/TheseusTab.tsx
@@ -0,0 +1,45 @@
+"use client";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Tab from "@mui/material/Tab";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import AreaChartPoolsApex from "../charts/poolCharts/AreaChartPoolsApex";
+export default function TheseusTab() {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%", typography: "body1" }}>
+      <TabContext value={value}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            variant="scrollable"
+            textColor="inherit"
+          >
+            <Tab className="text-white" label="Insurance Fund" value="1" />
+            <Tab className="text-white" label="Pool Earnings" value="2" />
+            <Tab className="text-white" label="Staked in Pools" value="3" />
+            <Tab className="text-white" label="Amount on Loan" value="4" />
+            <Tab className="text-white" label="Dividend Payments" value="5" />
+            <Tab className="text-white" label="DAO Earnings" value="6" />
+          </TabList>
+        </Box>
+        <TabPanel value="1">
+          <AreaChartPoolsApex />
+        </TabPanel>
+        <TabPanel value="2">Graph no</TabPanel>
+        <TabPanel value="3">Item Three</TabPanel>
+        <TabPanel value="4">Item Three</TabPanel>
+        <TabPanel value="5">Item Three</TabPanel>
+        <TabPanel value="6">Item Three</TabPanel>
+      </TabContext>
+    </Box>
+  );
+}
