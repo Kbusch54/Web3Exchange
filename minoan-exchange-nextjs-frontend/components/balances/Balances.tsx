65f662d68cdb73707a50d58f7f920036e67dee0e
Kevin Busch <kevinbusch@comcast.net>
2023-04-18 17:49:35 -0400

    shorted classes and left comments

--- minoan-exchange-nextjs-frontend/components/balances/Balances.tsx
+++ minoan-exchange-nextjs-frontend/components/balances/Balances.tsx
@@ -10,27 +10,27 @@ const Balances: React.FC<Props> = () => {
         id={"balances"}
         className="balance-container"
       >
-        <div className="balance-box">
+        <div>
           <h1>0.00</h1>
           <h3> Your Balance</h3>
         </div>
-        <div className="balance-box">
+        <div>
           <h1>$0.00</h1>
           <h3> Current Value</h3>
         </div>
-        <div className="balance-box">
+        <div>
           <h1>134533</h1>
           <h3> Total Supply</h3>
         </div>
-        <div className="balance-box">
+        <div>
           <h1>$9382.02</h1>
           <h3> Total Value</h3>
         </div>
-        <div className="balance-box">
+        <div>
           <h1>$6983.39</h1>
           <h3> Loaned Out</h3>
         </div>
-        <div className="balance-box">
+        <div>
           <h1>$2398.63</h1>
           <h3> In Vault</h3>
         </div>
