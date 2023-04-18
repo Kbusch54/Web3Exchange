d7d06decd7625eb0d3f1a402d0f97968165632bc
Kevin Busch <kevinbusch@comcast.net>
2023-04-18 17:41:15 -0400

    components 4 balance, invest/stakeStats CSSclasses

--- minoan-exchange-nextjs-frontend/app/pools/[slug]/page.tsx
+++ minoan-exchange-nextjs-frontend/app/pools/[slug]/page.tsx
@@ -7,6 +7,10 @@ import dynamic from "next/dynamic";
 import DaoTransaction from "../../../components/tables/DaoTransactions";
 import PurposalModal from "../../../components/modals/PurposalModal";
 import StakingForm from "../../../components/forms/StakingForm";
+import Balance from "../../../components/balances/Balances";
+import Balances from "../../../components/balances/Balances";
+import InvestorStats from "../../../components/stockData/InvestorStats";
+import StakingStats from "../../../components/stockData/StakingStats";
 
 interface Props {
   params: {
@@ -50,35 +54,7 @@ export default async function PoolPage({ params }: Props) {
               <h1>{stock.name}</h1>
               <h3 className="text-xl">{stock.symbol}</h3>
             </div>
-            <section
-              id={"balances"}
-              className=" col-span-6 md:col-span-4 lg:col-span-6 lg:mt-0 grid grid-cols-2 xl:grid-cols-3 mt-12 gap-y-12"
-            >
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">0.00</h1>
-                <h3 className="text-xl"> Your Balance</h3>
-              </div>
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">$0.00</h1>
-                <h3 className="text-xl"> Current Value</h3>
-              </div>
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">134533</h1>
-                <h3 className="text-xl"> Total Supply</h3>
-              </div>
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">$9382.02</h1>
-                <h3 className="text-xl"> Total Value</h3>
-              </div>
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">$6983.39</h1>
-                <h3 className="text-xl"> Loaned Out</h3>
-              </div>
-              <div className="flex flex-col text-center">
-                <h1 className="text-xl md:text-3xl lg:text-5xl">$2398.63</h1>
-                <h3 className="text-xl"> In Vault</h3>
-              </div>
-            </section>{" "}
+            <Balances />
             <section
               id={"charts"}
               className="hidden md:block col-span-9  shadow-xl shadow-slate-500"
@@ -93,70 +69,10 @@ export default async function PoolPage({ params }: Props) {
             </section>
             <section
               id={"stats"}
-              className="col-span-9 lg:col-span-9 flex flex-wrap justify-evenly text-center gap-y-12"
+              className="col-span-9 lg:col-span-9 flex flex-wrap justify-evenly items-center text-center gap-y-12"
             >
-              <div className="rounded-2xl bg-gradient-to-r from-cyan-500 via-red-500 to-amber-500 p-1 shadow-xl">
-                <div className="block rounded-xl bg-slate-800 -z-10 p-4 sm:p-6 lg:p-8">
-                  <div className="flex flex-col gap-y-4 ">
-                    <h1>Investor Stats</h1>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Minimum Margin Ratio:</h3>
-                      <h3>5%</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Max Leverage:</h3>
-                      <h3>15X</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Minimum Investment:</h3>
-                      <h3>$2,000.03</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Maximum Investment:</h3>
-                      <h3>$87,039.25</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Interest Rate:</h3>
-                      <h3>3.2 %</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Interest Payment Period:</h3>
-                      <h3>8 hrs</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="rounded-2xl bg-gradient-to-r from-cyan-500 via-red-500 to-amber-500 p-1 shadow-xl">
-                <div className="block rounded-xl bg-slate-800 -z-10 p-4 sm:p-6 lg:p-8">
-                  <div className="flex flex-col gap-y-4">
-                    <h1>Staking Information</h1>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Stakers:</h3>
-                      <h3>126</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>USDC to Tok:</h3>
-                      <h3>12:3</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>ROI (72h) avg:</h3>
-                      <h3>$475.02</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Reward %:</h3>
-                      <h3>48 %</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Reward Period:</h3>
-                      <h3>96 hrs</h3>
-                    </div>
-                    <div className="flex flex-row gap-x-24 text-lg text-center justify-between">
-                      <h3>Minimum Stake:</h3>
-                      <h3>$2.05</h3>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <InvestorStats />
+              <StakingStats />
             </section>
             <section
               id={"staking"}
