8fcb95489356a41d0111adfd30248ee728a5b1a5
Kevin Busch <kevinbusch@comcast.net>
2023-06-07 11:39:13 -0400

    loading state for dashabord assets

---
+++ minoan-exchange-nextjs-frontend/components/balances/dashboard/LoadingState.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const LoadingState: React.FC<Props> = () => {
+    return (
+        <table className='lg:mt-0 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 justify-center align-middle  mt-12 gap-y-6 gap-x-6 text-white'>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                    <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-xl bg-sky-800  bg-opacity-40'>
+                        <h1>Loading</h1>
+                        <h3>...</h3>
+                    </div>
+                </table>
+
+    )
+}
+
+export default LoadingState
