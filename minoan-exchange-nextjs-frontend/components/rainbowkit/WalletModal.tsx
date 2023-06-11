e32a87b4257179b9607980f56691a857332f64d9
Kevin Busch <kevinbusch@comcast.net>
2023-06-11 17:02:12 -0400

    wallet modal avatar pic, copy button

--- minoan-exchange-nextjs-frontend/components/rainbowkit/WalletModal.tsx
+++ minoan-exchange-nextjs-frontend/components/rainbowkit/WalletModal.tsx
@@ -1,6 +1,11 @@
 'use client'
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import Modal from 'react-modal';
+import { useSession } from 'next-auth/react';
+import Countdown from '../countdowns/Countdown';
+import Image from 'next/image';
+import Copy from '../utils/Copy';
+
 
 interface Props {
     account: account,
@@ -38,7 +43,9 @@ const customStyles = {
 
 };
 const WalletModal: React.FC<Props> = ({ account, signOutFunc }) => {
+    const session = useSession();
     const [modalIsOpen, setIsOpen] = useState(false);
+    const [bannerbearImage, setBannerbearImage] = useState<any>(null);
 
     function openModal() {
         setIsOpen(true);
@@ -52,12 +59,14 @@ const WalletModal: React.FC<Props> = ({ account, signOutFunc }) => {
     function closeModal() {
         setIsOpen(false);
     }
+
+
     return (
         <div className='flex flex-row gap-x-1 md:gap-x-4  '>
             <button className='px-2 py-1 md:px-3 md:py-2  text-white border rounded-xl hover:scale-125 bg-blue-600 text-sm md:text-md lg:text-lg 2xl:text-xs 3xl:text-xl' onClick={openModal}>
                 Open Modal
             </button>
-                <button className='px-2 py-1 md:px-3 md:py-2  bg-red-400 text-white border rounded-xl hover:scale-125 text-sm md:text-md lg:text-lg 2xl:text-xs 3xl:text-xl' onClick={signOutFunc}>Sign Out</button>
+            <button className='px-2 py-1 md:px-3 md:py-2  bg-red-400 text-white border rounded-xl hover:scale-125 text-sm md:text-md lg:text-lg 2xl:text-xs 3xl:text-xl' onClick={signOutFunc}>Sign Out</button>
             <Modal
                 isOpen={modalIsOpen}
                 onAfterOpen={afterOpenModal}
@@ -67,21 +76,39 @@ const WalletModal: React.FC<Props> = ({ account, signOutFunc }) => {
                 contentLabel="Wallet Modal"
             >
                 <div className='flex flex-col justify-center gap-y-4 bg-slate-800 p-12 rounded-xl relative'>
-                    <p className='font-bold mb-2 bg-amber-400 p-2 rounded-xl'>{account.address}</p>
+                    <div className='object-scale-down self-center bg-gray-600 rounded-full p-4'>
+                        {account.address ? (
+                            <Image src={`https://avatars.dicebear.com/api/bottts/${account.address}.svg`} height={100} width={100} alt="avatar" />
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                    <p className='font-bold mb-2 bg-amber-400 p-2 rounded-xl text-gray-800'>{account.address}</p>
                     <button onClick={closeModal} className='bg-gray-200 rounded-full px-4 py-1 text-xl text-gray-600 absolute right-2 top-2'>x</button>
-                    
-                    <div className='flex flex-row justify-around gap-x-12 text-amber-400'>
-                        <div className='flex flex-row bg-slate-700 rounded-xl py-4'>
-                            <p>{account.displayName}</p>
-                            <button>Copy</button>
+
+                    <div className='flex flex-row justify-between  text-amber-400 mx-12'>
+                        {account.displayName && account.address && (
+                            <div className='flex flex-row bg-slate-700 rounded-xl py-4 gap-x-2'>
+                                <p>{account.displayName}</p>
+                            <Copy toCopy={account.address.toString()} size={25}/>
                         </div>
+                        )}
                         <p className='bg-slate-700 rounded-xl py-4'>{account.displayBalance}</p>
                     </div>
+                    {session.data?.expires && (
+                        <div>
+                            <p className='text-white text-center'>Session Expires in:</p>
+                            <div className='text-3xl text-center text-amber-400 '>
+                                <Countdown targetDate={new Date(session.data?.expires)} />
+                            </div>
+                        </div>
+                    )}
+
                     <button className='px-3 py-2  bg-red-400 text-xl text-white border rounded-xl hover:scale-125 ' onClick={signOutFunc} >Sign Out</button>
                 </div>
 
-            </Modal>
-        </div>
+            </Modal >
+        </div >
     )
 }
 
