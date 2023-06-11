6a396949f08c3d994b017225b714641739f52d12
Kevin Busch <kevinbusch@comcast.net>
2023-06-11 18:02:16 -0400

    moved file to folder

--- minoan-exchange-nextjs-frontend/app/api/auth/[...nextauth]/route.ts
+++ minoan-exchange-nextjs-frontend/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import { authOptions } from "../../../../utils/authOptions";
+import { authOptions } from "../../../../utils/auth/authOptions";
 // import { ethers } from "ethers";
 
 const handler = NextAuth(authOptions);
