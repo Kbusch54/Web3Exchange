d6d4ddf8ce2675a10c5b2f533783704b6e31afdb
Kevin Busch <kevinbusch@comcast.net>
2023-06-12 18:45:18 -0400

    theseus tab, and new chart

---
+++ minoan-exchange-nextjs-frontend/components/charts/poolCharts/recharts/RechartDoubleBar.tsx
@@ -0,0 +1,85 @@
+'use client'
+import React from 'react'
+import { ResponsiveContainer, BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Bar, Cell } from 'recharts'
+
+interface Props {
+    
+}
+const data = [
+    {
+      name: 'Theseus Dao',
+      proposals: 26,
+      executed: 10,
+    },
+    {
+      name: 'Ariadne Tesla Dao',
+      proposals: 20,
+      executed: 19,
+    },
+    {
+      name: 'Ariadne Google Dao',
+      proposals: 37,
+      executed: 15,
+    },
+    {
+      name: 'Ariadne Meta Dao',
+      proposals: 12,
+      executed: 4,
+    },
+  ];
+  function dataMutate(data: any) {
+    let newData: {name:string,proposals:number,executed:number,proposal:number}[] = [];
+    data.map((entry: {name:string,proposals:number,executed:number}, index: number) => ({
+            newData: newData.push({
+                name: entry.name,
+                proposal: Number(entry.proposals) -Number(entry.executed),
+                proposals:entry.proposals,
+                executed: entry.executed,
+            })
+        }));
+        return newData;
+    }
+//@ts-ignore
+  function CustomTooltip({ active, payload, label }) {
+      if (active) {
+        return (
+        <div className="tooltip ">
+            <h4>{label}</h4>
+            <p>{payload[0].payload.proposals} Proposals</p>
+            <p>{payload[0].payload.executed} Executed</p>
+            <p>{(payload[0].payload.executed/payload[0].payload.proposals *100).toFixed(2)} % Passed</p>
+        </div>
+        );
+    }
+    return null;
+  }
+
+const RechartDoubleBar: React.FC<Props> = () => {
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <BarChart
+            width={500}
+            height={300}
+            data={dataMutate(data)}
+            margin={{
+                top: 20,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                {/* @ts-ignore */}
+                <Tooltip content={<CustomTooltip/>} />
+                <Legend />
+               
+                <Bar dataKey="executed" stackId="a" fill="rgb(217 119 6 )" />
+                <Bar dataKey='proposal'stackId="a"  fill=' rgb(30 41 59  )' />
+            </BarChart>
+      </ResponsiveContainer>
+    )
+}
+
+export default RechartDoubleBar
--- minoan-exchange-nextjs-frontend/components/tabs/TheseusTab.tsx
+++ minoan-exchange-nextjs-frontend/components/tabs/TheseusTab.tsx
@@ -5,7 +5,10 @@ import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
-import AreaChartPoolsApex from "../charts/poolCharts/AreaChartPoolsApex";
+import ReachartsEx from "../charts/poolCharts/ReachartsEx";
+import RechartPie from "../charts/poolCharts/recharts/RechartPie";
+import ReachartLines from "../charts/poolCharts/recharts/RechartLines";
+import RechartDoubleBar from "../charts/poolCharts/recharts/RechartDoubleBar";
 export default function TheseusTab() {
   const [value, setValue] = React.useState("1");
 
@@ -26,20 +29,43 @@ export default function TheseusTab() {
             <Tab className="text-white" label="Insurance Fund" value="1" />
             <Tab className="text-white" label="Pool Earnings" value="2" />
             <Tab className="text-white" label="Staked in Pools" value="3" />
-            <Tab className="text-white" label="Amount on Loan" value="4" />
-            <Tab className="text-white" label="Dividend Payments" value="5" />
-            <Tab className="text-white" label="DAO Earnings" value="6" />
+            <Tab className="text-white" label="Long vs Shorts" value="4" />
+            <Tab className="text-white" label="Trades" value="5" />
+            <Tab className="text-white" label="Proposals" value="6" />
+            <Tab className="text-white" label="Avg Pnl" value="7" />
           </TabList>
         </Box>
         <TabPanel value="1">
-          {/* <AreaChartPoolsApex /> */}
-          hello
+          <ReachartsEx height={400} />
         </TabPanel>
-        <TabPanel value="2">Graph no</TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
-        <TabPanel value="4">Item Three</TabPanel>
-        <TabPanel value="5">Item Three</TabPanel>
-        <TabPanel value="6">Item Three</TabPanel>
+        <TabPanel value="2">
+          <ReachartsEx height={400} />
+        </TabPanel>
+        <TabPanel value="3">
+          <RechartPie />
+        </TabPanel>
+        <TabPanel value="4">
+          <RechartPie />
+        </TabPanel>
+        <TabPanel value="5">
+          <div className="grid grid-cols-4 items-center mx-2">
+            <div className=" hidden  lg:inline lg:col-span-3">
+              <ReachartLines height={400} />
+            </div>
+            <div className="col-span-4 lg:hidden">
+              <ReachartLines height={200} />
+            </div>
+            <div className="col-span-4 lg:col-span-1">
+              <RechartPie />
+            </div>
+          </div>
+        </TabPanel>
+        <TabPanel value="6">
+          <RechartDoubleBar />
+        </TabPanel>
+        <TabPanel value="7">
+          <ReachartLines height={400} />
+        </TabPanel>
       </TabContext>
     </Box>
   );
