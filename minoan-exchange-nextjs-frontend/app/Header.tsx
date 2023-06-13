15e533ae60ceb4f188d88852164fc820e4cff628
Kevin Busch <kevinbusch@comcast.net>
2023-06-13 14:25:04 -0400

    navigation button hamburger

--- minoan-exchange-nextjs-frontend/app/Header.tsx
+++ minoan-exchange-nextjs-frontend/app/Header.tsx
@@ -3,19 +3,27 @@ import React from "react";
 import Image from "next/image";
 import logo from "../public/assets/minotaur-logo-thing.png";
 import ConnectingButton from "../components/rainbowkit/ConnectingButton";
+import NavButton from "../components/utils/nav/NavButton";
 
 interface Props { }
 
 const Header: React.FC<Props> = () => {
   return (
     <header className=" bg-slate-700 ">
+      
       <div className="flex gap-x-4 text-white justify-between align-center mx-6">
 
-        <a href="/" className="p-2  flex flex-row ">
-          <Image src={logo} alt="logo" height={20} width={35} />
-          <h1 className="inline md:hidden ml-14 text-2xl text-white align-middle text-left">HH</h1>
-        </a>
-        <div className=" hidden md:flex gap-x-14 lg:gap-x-20 xl:gap-x-32 2xl:gap-x-64 mt-4 text-lg  ">
+        <div className="flex flex-row justify-around gap-x-6 align-middle items-center">
+
+          <a href="/" className="p-2  flex align-middle items-center ">
+            <Image src={logo} alt="logo" height={20} width={35} />
+          </a>
+          <div className="block lg:hidden  ">
+
+            <NavButton />
+          </div>
+        </div>
+        <div className=" hidden lg:flex  gap-x-14 lg:gap-x-20 xl:gap-x-32 2xl:gap-x-64 mt-4 text-lg  ">
           <a href="/todos">
             Docs
           </a>
