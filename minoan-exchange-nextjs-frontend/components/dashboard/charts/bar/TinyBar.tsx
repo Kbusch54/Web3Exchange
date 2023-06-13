e02a0655d5c39b7e6be3a1ba91f6d559e934be8d
Kevin Busch <kevinbusch@comcast.net>
2023-06-13 10:51:51 -0400

    select optionjs for chart compoinents

--- minoan-exchange-nextjs-frontend/components/dashboard/charts/bar/TinyBar.tsx
+++ minoan-exchange-nextjs-frontend/components/dashboard/charts/bar/TinyBar.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import RechartTinyBar from '../../../charts/poolCharts/recharts/RechartTinyBar'
-
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 interface Props {
 
 }
 
 const TinyBar: React.FC<Props> = () => {
+    const [selected, setSelected] = useState(false)
+    const [graphType, setGraphType] = useState(0)
+    useEffect(() => {
+        setSelected(prev => false)
+    }, [graphType])
     return (
         <div className='col-span-6 2xl:col-span-2 self-center mx-16 mt-4 md:mx-40 md:mt-6 2xl:mx-0 2xl:mt-0 2xl:pr-4'>
-            <div className='flex flex-row justify-between bg-slate-800 rounded-2xl px-4 my-4'>
-                <p className='text-white text-3xl text-center p-4'>PNL</p>
-                <button className='text-white text-3xl text-center p-4'>d</button>
+            <div className='flex flex-row justify-between bg-slate-800 rounded-2xl px-4 my-4 relative'>
+                <p className='text-white text-3xl text-center p-4'>{graphType == 0 ? 'PNL' : "Trade Duration"}</p>
+                <button className={`text-3xl text-white ${selected ? 'hidden' : 'block'}`} onClick={() => setSelected(prev => !prev)} >
+                    <KeyboardArrowDownIcon height={50} />
+                </button>
+                <button className={`text-3xl text-white ${!selected ? 'hidden' : 'block'}`} onClick={() => setSelected(prev => !prev)}>
+                    <KeyboardArrowUpIcon height={50} />
+                </button>
+                <div className={`absolute right-0 top-24 w-full  ${selected?'grid':'hidden'} bg-slate-800 border-2 border-slate-700  grid-cols-2   `} >
+                    <button onClick={()=>setGraphType(0)} className={` col-span-1 text-lg text-center ${graphType == 1?'text-white hover:text-slate-800 hover:bg-white  hover:scale-110':'text-slate-800 bg-white border-4 border-slate-900 scale-90'}`}>PNL</button>
+                    <button onClick={()=>setGraphType(1)} className={` col-span-1 text-lg text-center ${graphType == 0?'text-white hover:text-slate-800 hover:bg-white  hover:scale-110':'text-slate-800 bg-white border-4 border-slate-900 scale-90'}`}>Trade Durations</button>
+                </div>
             </div>
             <div className=' flex flex-col flex-wrap bg-slate-800 rounded-2xl '>
                 <div className='flex flex-row justify-between align-middle place-items-center px-2 mt-8 text-white text-2xl text-center'>
-                    <p className=''>Earnings</p>
+                    <p className=''>{graphType == 0 ? 'Earnings' : "Avg Duration"}</p>
                     <p className='text-amber-400 text-lg'>+$200.77</p>
                 </div>
                 <RechartTinyBar height={270} />
--- minoan-exchange-nextjs-frontend/components/dashboard/charts/pies/PieBox.tsx
+++ minoan-exchange-nextjs-frontend/components/dashboard/charts/pies/PieBox.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import RechartPie from '../../../charts/poolCharts/recharts/RechartPie'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 interface Props {
-    
+
 }
 
 const PieBox: React.FC<Props> = () => {
+    const [selected, setSelected] = useState(false)
+    const [graphType, setGraphType] = useState(0)
+    useEffect(() => {
+        setSelected(prev=>false)
+    }, [graphType])
     return (
         <div className='col-span-12 mx-0 md:mx-12 lg:col-span-6 2xl:col-span-4 3xl-col-span-3 px-8'>
-        <div className='h-16 w-1/2 rounded-tr-2xl bg-slate-800  border-b border-blue-300'>
-            <p className='text-white text-3xl text-center pt-4'>Investments</p>
-        </div>
-        <div className='text-white text-3xl w-1/2 bg-[rgba(24,24,35,255)]'></div>
-        <div className='bg-slate-800 flex flex-col flex-wrap rounded-2xl rounded-tl-none'>
-            <RechartPie />
+            <div className='h-16 w-1/2 rounded-tr-2xl bg-slate-800  border-b border-blue-300 relative'>
+                <div className='flex flex-row justify-between mx-6 lg:mx-0 3xl:mx-4'>
 
+                    <p className='text-white text-3xl pt-4'>{graphType == 0 ? 'Investments' : "Staking"}</p>
+                    <button className={`text-3xl text-white ${selected ? 'hidden' : 'block'}`} onClick={() => setSelected(prev => !prev)} >
+                        <KeyboardArrowDownIcon height={50} />
+                    </button>
+                    <button className={`text-3xl text-white ${!selected ? 'hidden' : 'block'}`} onClick={() => setSelected(prev => !prev)}>
+                        <KeyboardArrowUpIcon height={50} />
+                    </button>
+                </div>
+                <div className={`absolute right-0 w-full  ${selected?'grid':'hidden'} bg-slate-800 border-2 border-slate-700  grid-cols-2   `} >
+                    <button onClick={()=>setGraphType(1)} className={` col-span-1 text-xl text-center ${graphType == 0?'text-white hover:text-slate-800 hover:bg-white  hover:scale-110':'text-slate-800 bg-white border-4 border-slate-900 scale-90'}`}>Staking</button>
+                    <button onClick={()=>setGraphType(0)} className={` col-span-1 text-xl text-center ${graphType == 1?'text-white hover:text-slate-800 hover:bg-white  hover:scale-110':'text-slate-800 bg-white border-4 border-slate-900 scale-90'}`}>Investments</button>
+                </div>
+            </div>
+            <div className='text-white text-3xl w-1/2 bg-[rgba(24,24,35,255)]'></div>
+            <div className='bg-slate-800 flex flex-col flex-wrap rounded-2xl rounded-tl-none'>
+                <RechartPie />
+
+            </div>
         </div>
-    </div>
     )
 }
 
