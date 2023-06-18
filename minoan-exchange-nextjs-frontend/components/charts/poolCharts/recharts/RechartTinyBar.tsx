3688fe84f23d4682dbdb69a773701a9ca27b21ae
Kevin Busch <kevinbusch@comcast.net>
2023-06-18 17:49:28 -0400

    added dynamic options

--- minoan-exchange-nextjs-frontend/components/charts/poolCharts/recharts/RechartTinyBar.tsx
+++ minoan-exchange-nextjs-frontend/components/charts/poolCharts/recharts/RechartTinyBar.tsx
@@ -15,6 +15,7 @@ import {
     Cell,
   } from "recharts";
   import { format, parseISO, subDays } from "date-fns";
+import { getHoursAndMinutes, moneyFormatter } from "utils/helpers/functions";
   
   const data: any[] | undefined = [];
   for (let num = 8; num >= 0; num--) {
@@ -29,29 +30,34 @@ import {
 
 interface Props {
     height: number
+    dataBar?: any
+    toolTipLabel?: string
+    toolTipPost?: string
     
 }
 const COLORS = ["#2451B7","#9251B7", "rgb(30 58 138)", "rgb(88 28 135)",  "rgb(153 27 27)"];
 //@ts-ignore
-function CustomTooltip({ active, payload, label }) {
+function CustomTooltip({ active, payload, label,toolTipLabel,tooTipPost }) {
     if (active) {
+      const date = new Date(Number(payload[0].payload.date) * 1000);
       return (
         <div className="tooltip ">
-          {/* <h4>{format(parseISO(label), "eeee, d MMM, yyyy")}</h4> */}
-          <p>${payload[0].value.toFixed(2)} PNL</p>
+          <h4>{date.toISOString().substring(0, 10)
+          }</h4>
+          <p>{toolTipLabel?toolTipLabel:''}{toolTipLabel == '$'?moneyFormatter(payload[0].value):toolTipLabel == 'Duration: '? getHoursAndMinutes(payload[0].value * 1000)[0]: payload[0].value} {tooTipPost}</p>
         </div>
       );
     }
     return null;
   }
 
-const RechartTinyBar: React.FC<Props> = ({height}) => {
+const RechartTinyBar: React.FC<Props> = ({height,dataBar,toolTipLabel,toolTipPost}) => {
     return (
       // <div className="h-max w-96">
       <ResponsiveContainer width="100%" height={height}>
       <BarChart
 
-     data={data}
+     data={dataBar?dataBar:data}
      margin={{
        top: 50,
        right: 10,
@@ -62,7 +68,7 @@ const RechartTinyBar: React.FC<Props> = ({height}) => {
       >
         {/* <CartesianGrid  className="text-red-700"/> */}
                   {/* @ts-ignore */}
-        <Tooltip content={<CustomTooltip />} />
+        <Tooltip content={<CustomTooltip tooTipPost={toolTipPost} toolTipLabel={toolTipLabel} />} />
         <Bar dataKey="value" background={{ fill: 'rgb(51 65 85)' }}     barSize={4} >
         {data.map((item, index) => (
             <Cell key={index} fill={item.value >= 0 ? 'rgb(56 189 247)' : 'rgb(185 28 28)'} />
