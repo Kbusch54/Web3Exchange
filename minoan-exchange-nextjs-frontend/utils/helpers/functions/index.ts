94c68030e502e8f13aa346bbbb08f0fdac7ed619
Kevin Busch <kevinbusch@comcast.net>
2023-06-18 17:48:05 -0400

    added gethoursandmin fuction

--- minoan-exchange-nextjs-frontend/utils/helpers/functions/index.ts
+++ minoan-exchange-nextjs-frontend/utils/helpers/functions/index.ts
@@ -11,3 +11,12 @@ export const convertCamelCaseToTitle = (camelCaseString: string) => {
 export const moneyFormatter = (num: number) => {
     return (num/10**6).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+function padTo2Digits(num:number) {
+    return num.toString().padStart(2, '0');
+  }
+export const getHoursAndMinutes = (totalMinutes: number) => {
+    const totalTime = totalMinutes/ 360000*6;
+    const minutes = totalTime % 60;
+    const hours = Math.floor(totalTime / 60);
+    return [`${hours}h${minutes > 0 ? ` ${Math.floor(minutes)}m ` : ''}`, `${padTo2Digits(hours)}:${padTo2Digits(Math.floor(minutes))}`,totalTime];
+}
