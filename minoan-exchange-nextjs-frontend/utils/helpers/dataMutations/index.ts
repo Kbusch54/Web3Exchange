2cc02241a2eec5ac0c14b1c0b4c3d28f7282cb01
Kevin Busch <kevinbusch@comcast.net>
2023-06-19 13:15:53 -0400

    get trade history function

--- minoan-exchange-nextjs-frontend/utils/helpers/dataMutations/index.ts
+++ minoan-exchange-nextjs-frontend/utils/helpers/dataMutations/index.ts
@@ -1,4 +1,5 @@
 import { Address } from "wagmi";
+import { getAmmName } from "../doas";
 
 export const getPNlByUser = (trades: any, user: Address,newArrLength:number) => {
     let pnl :{ date: string; value: number; }[] = [];
@@ -22,4 +23,35 @@ export const getTradeDurationByUser = (trades: any, user: Address,newArrLength:n
     }
     avg = avg/duration.length;
     return {duration,avg};
+}
+
+export const getTradeHistory = (trades: any, user: Address) => {
+    let data: { date: string; Tesla: number; Google: number; Meta: number; All: number; }[] = [];
+    let dateMap = new Map();
+    let leastDate;
+    for (let i = 0; i <trades.length; i++) {
+        if(trades[i].user.id.toLowerCase() !== user.toLowerCase() || trades[i].isActive == true) continue;
+        let created = new Date((trades[i].created * 1000) ).toISOString().substring(0, 10);
+        if(!leastDate) leastDate = created;
+        if(leastDate > created) leastDate = created;
+        if(dateMap.has(created)){
+            let temp = dateMap.get(created);
+            temp[getAmmName(trades[i].ammPool.id) as keyof typeof temp] ++;
+            temp['All'] ++;
+            dateMap.set(created,temp);
+        }else{
+            let temp: { "Tesla": number; "Google": number; "Meta": number; "All": number; }= {"Tesla":0,"Google":0,"Meta":0,"All":0};
+            let ammName = getAmmName(trades[i].ammPool.id);
+            temp[ammName as keyof typeof temp] ++;
+            temp['All'] ++;
+            dateMap.set(created,temp);
+        }   
+    }
+    // @ts-ignore
+    data.push({date:leastDate,Tesla:0,Google:0,Meta:0,All:0})
+    dateMap.forEach((value, key) => {
+        data.push({date:key,Tesla:value.Tesla,Google:value.Google,Meta:value.Meta,All:value.All})
+    });
+    console.log(data);
+    return data;
 }
