d11790e2cb2477ad3a0cf8d6fead54ff569b9a52
Kevin Busch <kevinbusch@comcast.net>
2023-06-20 19:56:20 -0400

    updated to incorporate new functions

--- minoan-exchange-nextjs-frontend/components/menus/TypeSelection.tsx
+++ minoan-exchange-nextjs-frontend/components/menus/TypeSelection.tsx
@@ -1,10 +1,11 @@
 'use client'
-import React, { useState } from 'react'
+import React, { use, useState } from 'react'
 import SelectType from './options/selectTypeOptions';
 import ReachartLines from '../charts/poolCharts/recharts/RechartLines';
 import RechartPie from '../charts/poolCharts/recharts/RechartPie';
 import { Address } from 'wagmi';
-import { getTardeSidesByAmm, getTradeHistory } from 'utils/helpers/dataMutations';
+import { getProposalSignersByAmm, getProposalTime, getProposalsByAmm, getTardeSidesByAmm, getTradeHistory } from 'utils/helpers/dataMutations';
+import { getHoursAndMinutes } from 'utils/helpers/functions';
 
 interface Props {
     poolData: any
@@ -16,15 +17,24 @@ const TypeSelection: React.FC<Props> = ({poolData,user}) => {
     const handleChange = (newType: string) => {
         setType(prevState => newType)
     }
+    console.log('this is pool data',poolData)
     const poolBalances = poolData.vamms[0].loanPool.poolBalance;
     const trades = poolData.trades;
+    const proposals = poolData.proposals;
+    console.log('this is proposals',proposals);
     const poolDataForPie = [
         { name: 'Available USDC', value: Number(poolBalances.availableUsdc)/10**6 },
         { name: 'Outstanding Loan USDC', value: Number(poolBalances.outstandingLoanUsdc)/10**6 },
     ]
     const pieDataForTrading = getTardeSidesByAmm(trades,poolData.vamms[0].loanPool.id);
     const lineDataForTrading = getTradeHistory(trades,undefined,poolData.vamms[0].loanPool.id);
+    const lineDataForProposal = getProposalsByAmm(proposals,poolData.vamms[0].loanPool.id);
+    const avgProposalTime = getProposalTime(proposals,poolData.vamms[0].loanPool.id);
+    const avgSigners = use(getProposalSignersByAmm(poolData.vamms[0].loanPool.id));
+    console.log('this is avg signers',avgSigners);
+    console.log('this is avg proposal time',avgProposalTime);
 
+    console.log('this is lineDataForProposal',lineDataForProposal);
     return (
         <div className='flex flex-col justify-center self-center'>
             <div>
@@ -100,19 +110,19 @@ const TypeSelection: React.FC<Props> = ({poolData,user}) => {
                   <div className="grid grid-cols-7 lg:grid-cols-12 gap-x-8  items-center">
                   <div className='col-span-7 flex-col inline-flex'>
                       <div>
-                          {/* <ReachartLines height={400} /> */}
+                          <ReachartLines height={400}  lineData={lineDataForProposal} type='time' />
                       </div>
                       <div className='flex flex-row justify-around pt-10'>
                           <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-lg bg-sky-800 m-4 bg-opacity-40 p-4'>
-                              <h1 className=' lg:text-5xl mt-4'>${20}</h1>
+                              <h1 className=' lg:text-5xl mt-4'>{avgSigners[1]}</h1>
                               <h3 className='text-xs md:text-lg'>Proposals</h3>
                           </div>
                           <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-lg bg-sky-800 m-4 bg-opacity-40 p-4'>
-                              <h1 className=' lg:text-5xl mt-4'>${20}</h1>
+                              <h1 className=' lg:text-5xl mt-4'>{avgSigners[0]}</h1>
                               <h3 className='text-xs md:text-lg'>Avg # Signers</h3>
                           </div>
                           <div className='flex flex-col text-center border-2 border-blue-800 rounded-t-2xl rounded-b-lg bg-sky-800 m-4 bg-opacity-40 p-4'>
-                              <h1 className=' lg:text-5xl mt-4'>${20}</h1>
+                              <h1 className=' lg:text-5xl mt-4'>{getHoursAndMinutes(avgProposalTime)[0].toString()}</h1>
                               <h3 className='text-xs md:text-lg'>Avg Proposal time</h3>
                           </div>
                       </div>
