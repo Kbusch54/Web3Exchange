086205a260e42c151a8b5e2a1e5d59f996393355
Kevin Busch <kevinbusch@comcast.net>
2023-06-21 11:05:23 -0400

    made new graph request

---
+++ minoan-exchange-nextjs-frontend/app/lib/graph/stakes.ts
@@ -0,0 +1,43 @@
+import request, { gql } from "graphql-request";
+import { cache } from 'react'
+
+export const fetchStakes = cache(async function getStakes() {
+    const query = gql`
+  query getstakes {
+    singleStakes {
+      user {
+        id
+      }
+      ammPool {
+        id
+      }
+      theseusDAO {
+        id
+      }
+      tokensMinted
+      usdcStaked
+      timeStamp
+    }
+    singleUnstakes {
+      user {
+        id
+      }
+      ammPool {
+        id
+      }
+      theseusDAO {
+        id
+      }
+      tokensBurned
+      usdcUnstaked
+      timeStamp
+    }
+  }
+`;
+
+
+
+const endpoint = "https://api.studio.thegraph.com/query/46803/subgraph-minoan/version/latest";
+const data = await request(endpoint, query);
+return data;
+});
