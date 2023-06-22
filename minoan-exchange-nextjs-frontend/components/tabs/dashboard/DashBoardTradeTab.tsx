5ecc433a1c4c966c61e2ddb14a0f98db13da69f2
Kevin Busch <kevinbusch@comcast.net>
2023-06-22 12:48:07 -0400

    no more suspense

--- minoan-exchange-nextjs-frontend/components/tabs/dashboard/DashBoardTradeTab.tsx
+++ minoan-exchange-nextjs-frontend/components/tabs/dashboard/DashBoardTradeTab.tsx
@@ -30,19 +30,15 @@ export default function DashBoardTradeTab({user}:Props) {
             variant="scrollable"
             textColor="inherit"
           >
-            <Tab className={`text-white `} classes={'text-white'} label="Active Trades" value="1" />
+            <Tab className={`text-white `}  label="Active Trades" value="1" />
             <Tab className="text-white" label="Past Trades" value="2" />
           </TabList>
         </Box>
         <TabPanel value="1">
-          <Suspense fallback={<ExSkeleton />}>
           <AllTrades user={user} userAvailableBalance={0} global={false} active={true}   />
-          </Suspense>
         </TabPanel>
         <TabPanel value="2">
-          <Suspense fallback={<ExSkeleton />}>
             <AllTrades user={user} userAvailableBalance={0} global={false} active={false}   />
-          </Suspense>
         </TabPanel>
       </TabContext>
     </Box>
