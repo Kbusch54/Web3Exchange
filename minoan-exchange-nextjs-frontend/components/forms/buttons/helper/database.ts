c4d04efc11fa1b3e21bc6db28afd822efc21fdcc
Kevin Busch <kevinbusch@comcast.net>
2023-06-22 12:37:03 -0400

    added gettransactions and change file name

---
+++ minoan-exchange-nextjs-frontend/components/forms/buttons/helper/database.ts
@@ -0,0 +1,19 @@
+import { cache } from "react";
+import { supabase } from "supabase";
+
+
+export const addTransaction = async (transactionHash: string, user: string, timestamp: string, name: string,type:string) => {
+const res = await supabase
+          .from('Past Transactions')
+          .insert([{transactionHash: transactionHash, user: user, timestamp: timestamp, name: name,type:type}]);
+          return res;
+}
+export const getTransactions = cache(async (user: string) => {
+    const { data, error } = await supabase
+            .from('Past Transactions')
+            .select()
+            .ilike('user', user);
+            console.log('data',data);
+            console.log('err',error);
+            return data;
+    });
