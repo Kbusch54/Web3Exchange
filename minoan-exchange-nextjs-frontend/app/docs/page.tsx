1f4e95153c2ec8481ee1dfc5514f85df4fe86ff1
Kevin Busch <kevinbusch@comcast.net>
2023-07-01 14:02:55 -0400

    rotes example

--- minoan-exchange-nextjs-frontend/app/docs/page.tsx
+++ minoan-exchange-nextjs-frontend/app/docs/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import React, { ReactElement } from "react";
 import FirstSection from "components/mainPage/FirstSection";
 import { revalidate } from "app/lib/graph/globalTradeData";
@@ -6,14 +6,31 @@ import { revalidate } from "app/lib/graph/globalTradeData";
 interface Props { }
 
 export default async function page({ }: Props) {
-  const data =  await fetch(`http://localhost:3000/api/tradeData`,{next:{revalidate:100000,tags:['tradeData']}})
+  const data =  await fetch(`http://localhost:3000/api/tradeData`,{next:{revalidate:8000}})
   const res = await data.json()
   // @ts-ignore
   console.log('data', res);
   return (
 
     <section className="text-center">
-      <FirstSection/>
+      {/* <FirstSection/> */}
+      <>
+      {res?(
+
+        res.map((mapValue:any,index:number)=>(
+          <div className="bg-amber-400 mt-64 inline-block mx-12 p-12 text-white " key={mapValue+index}>
+            <p>{mapValue.tradeOpenValues.openInterestRate}</p>
+            <p>{mapValue.tradeOpenValues.openLeverage}</p>
+            <p>{mapValue.tradeOpenValues.openEntryPrice}</p>
+          </div>
+        )
+        )
+        ):(
+          <div className="bg-amber-400 mt-64 text-white ">
+            <p>loading</p>
+          </div>
+        )}
+    </>
 
      
     </section>
