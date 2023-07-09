ed0b9743d37eab1a550ed1955ebce0c262d31db5
Kevin Busch <kevinbusch@comcast.net>
2023-07-09 12:19:32 -0400

    update to supabse config

--- minoan-exchange-nextjs-frontend/supabase.ts
+++ minoan-exchange-nextjs-frontend/supabase.ts
@@ -1,7 +1,6 @@
-// supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://daazbenqnrozlftxdunx.supabase.co'
-const SUPABASE_PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE||'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhYXpiZW5xbnJvemxmdHhkdW54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM2NDYyMzksImV4cCI6MTk5OTIyMjIzOX0.yUuYMMr3NITOUrcAn87xYCS49yxRbjMaCo1Utrd37Ro';
+const SUPABASE_PUBLIC_KEY = process.env.NEXT_PUBLIC_SUPABASE||'';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLIC_KEY);
